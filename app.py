07b82dc2197930ea14de6c40a7c53d23acc5e457
Glitchad <lichad3@gmail.com>
2024-05-15 08:44:51 +0200

    Froze

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
+import sys
+
 from flask import Flask, render_template
+from flask_frozen import Freezer
 
 app = Flask(__name__)
+freezer = Freezer(app)
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    if "freeze" in sys.argv:
+        freezer.freeze()
+    else:
+        app.run(debug=True)
